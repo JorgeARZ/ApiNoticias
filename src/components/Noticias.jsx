853842db19090c621f8da5ccf86edadd546aa8e9
gzuniga <gzuniga@economia.cl>
2023-08-30 16:34:32 -0400

    Version final

--- src/components/Noticias.jsx
+++ src/components/Noticias.jsx
@@ -1,49 +1,53 @@
-import {Card,CardActions,CardContent,CardMedia,Link,Typography,Grid}from '@mui/material'
+import Card from '@mui/material/Card'
+import CardActions from '@mui/material/CardActions'
+import CardContent from '@mui/material/CardContent'
+import CardMedia from '@mui/material/CardMedia'
+import Link from '@mui/material/Link'
+import Typography from '@mui/material/Typography'
+import Grid from '@mui/material/Grid'
 
 const Noticias = ({noticia}) => {
-    console.log(noticia)
-    const {urlToImage, url,title,description,source} = noticia
-  return (
-    <Grid item md={6} lg={4}>
-        <Card>
-           {urlToImage &&(
-                <CardMedia
-                    component="img"
-                    alt={`Imagen de la noticia ${title}`}
-                    image={urlToImage}
-                    height={'250'}
-                />
-           )}
 
-           <CardContent>
-                <Typography
-                    variant='h5' component='div'>
-                    {title}
-                </Typography>
+    const { urlToImage, url, title, description, source } = noticia
+    return (
+        <Grid item md={6} lg={4} >
+            <Card>
+                {urlToImage && (
+                    <CardMedia
+                        component="img"
+                        alt={`Imagen de la noticia ${title}`}
+                        image={urlToImage}
+                        height={'250'}
+                    />
+                )}
 
-                <Typography
-                    variant='body2'>
-                    {description}
-                </Typography>
+                <CardContent>
+                    <Typography variant='body1' color="error">
+                        {source.name}
+                    </Typography>
+                    <Typography variant='h5' component="div">
+                        {title}
+                    </Typography>
+                    <Typography variant='body2'>
+                        {description}
+                    </Typography>
+                </CardContent>
 
                 <CardActions>
                     <Link
                         href={url}
                         target="_blank"
-                        varaint='button'
-                        color={'primary'}
+                        variant='button'
                         width={'100%'}
                         textAlign={'center'}
                         sx={{
-                            textDecoration:'none'
+                            textDecoration: 'none'
                         }}
-                    >
-                    Leer Noticia</Link>
+                    >Leer Noticia</Link>
                 </CardActions>
-           </CardContent>
-        </Card>
-    </Grid>
-  )
+            </Card>
+        </Grid>
+    )
 }
 
 export default Noticias
