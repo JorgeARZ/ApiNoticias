1194c09d4f31207c6081fa25d9d7d5fe6ccfb346
JorgeARZ <jorgerojaszepedaa@gmail.com>
2023-08-30 17:34:21 -0400

    Version final 30-06

--- src/context/NoticiasProvider.jsx
+++ src/context/NoticiasProvider.jsx
@@ -12,7 +12,7 @@ const NoticiasProvider = ({children}) => {
 
     useEffect(() => {
         const consultarAPI = async () => {
-            const url = `https://newsapi.org/v2/top-headlines?country=us&category=${categoria}&apiKey=${import.meta.env.VITE_API_KEY}`
+            const url = `https://newsapi.org/v2/top-headlines?country=ar&category=${categoria}&apiKey=${import.meta.env.VITE_API_KEY}`
 
             const { data } = await axios(url)
             setNoticias(data.articles)
@@ -24,7 +24,7 @@ const NoticiasProvider = ({children}) => {
 
     useEffect(() => {
         const consultarAPI = async () => {
-            const url = `https://newsapi.org/v2/top-headlines?country=us&page=${pagina}&category=${categoria}&apiKey=${import.meta.env.VITE_API_KEY}`
+            const url = `https://newsapi.org/v2/top-headlines?country=ar&page=${pagina}&category=${categoria}&apiKey=${import.meta.env.VITE_API_KEY}`
 
             const { data } = await axios(url)
             setNoticias(data.articles)
